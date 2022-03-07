ef0251c9ffa6b12e7df0c3f67502f4b4bb320edf
DrgOv <62381817+DrgOv@users.noreply.github.com>
2022-03-07 16:35:25 +0200

    fixed topbar

--- src/Home.svelte
+++ src/Home.svelte
@@ -2,51 +2,53 @@
     import { Fullpage, FullpageSection } from 'svelte-fullpage';
     import { navigate } from 'svelte-routing';
     import { authStore } from './auth/authStore';
-    import { Btn } from './common/components';
+    import { Btn, Loading } from './common/components';
     import { TeamsList } from './team/components';
 
-    $: ({ user } = $authStore);
+    $: ({ user, isLoading } = $authStore);
 </script>
 
-<Fullpage>
-    <FullpageSection>
-        <div class="Home gray-overlay fullscreen centered">
-            <div class="Home__wrapper">
-                <h1>Iechipele</h1>
-                <h3>Gaseste playeri, rapid si simplu</h3>
+<Loading condition={isLoading}>
+    <Fullpage>
+        <FullpageSection>
+            <div class="Home gray-overlay fullscreen centered">
+                <div class="Home__wrapper">
+                    <h1>Iechipele</h1>
+                    <h3>Gaseste playeri, rapid si simplu</h3>
 
-                <div class="Home__wrapper__text">
-                    <p>
-                        Esti intr-o cautare disperata de oameni? Nu ai cu cine
-                        sa te joci? Prietenii te-au abandonat?
-                    </p>
-                    <p>
-                        Aici si acum poti sa te alaturi unor oameni alaturi de
-                        care sa iti dai in cap.
-                    </p>
-                </div>
-
-                {#if !user}
-                    <div>
-                        <Btn on:click={() => navigate('login')}>
-                            Intra in cont
-                        </Btn>
-                        <Btn on:click={() => navigate('register')}>
-                            Creeaza cont
-                        </Btn>
+                    <div class="Home__wrapper__text">
+                        <p>
+                            Esti intr-o cautare disperata de oameni? Nu ai cu
+                            cine sa te joci? Prietenii te-au abandonat?
+                        </p>
+                        <p>
+                            Aici si acum poti sa te alaturi unor oameni alaturi
+                            de care sa iti dai in cap.
+                        </p>
                     </div>
-                {/if}
-            </div>
-        </div>
-    </FullpageSection>
 
-    <FullpageSection>
-        <div class="header-offset margin-inline--s">
-            <h1>Echipe in cautare de playeri</h1>
-            <TeamsList />
-        </div>
-    </FullpageSection>
-</Fullpage>
+                    {#if !user}
+                        <div>
+                            <Btn on:click={() => navigate('login')}>
+                                Intra in cont
+                            </Btn>
+                            <Btn on:click={() => navigate('register')}>
+                                Creeaza cont
+                            </Btn>
+                        </div>
+                    {/if}
+                </div>
+            </div>
+        </FullpageSection>
+
+        <FullpageSection>
+            <div class="header-offset margin-inline--s">
+                <h1>Echipe in cautare de playeri</h1>
+                <TeamsList />
+            </div>
+        </FullpageSection>
+    </Fullpage>
+</Loading>
 
 <style>
     .Home {

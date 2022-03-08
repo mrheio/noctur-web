1b4bd7fe39674d9d2b70b93336a55b8f05528f37
DrgOv <62381817+DrgOv@users.noreply.github.com>
2022-03-08 17:59:11 +0200

    update

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,55 +1,40 @@
 <script>
-    import { Fullpage, FullpageSection } from 'svelte-fullpage';
     import { navigate } from 'svelte-routing';
     import { authStore } from './auth/authStore';
-    import { Btn, PageWrapper } from './common/components';
-    import { TeamsList } from './team/components';
+    import { Btn } from './common/components';
 
     $: ({ user, isLoading } = $authStore);
 </script>
 
-<Fullpage>
-    <FullpageSection>
-        <div class="Home gray-overlay fullscreen centered">
-            <div class="Home__content-wrapper">
-                <h1>Iechipele</h1>
-                <h3>Gaseste playeri, rapid si simplu</h3>
+<div class="Home gray-overlay fullscreen centered">
+    <div class="Home__content-wrapper">
+        <h1>Iechipele</h1>
+        <h3>Gaseste playeri rapid si simplu</h3>
 
-                <div class="Home__content-wrapper__text-wrapper">
-                    <p>
-                        Esti intr-o cautare disperata de oameni? Nu ai cu cine
-                        sa te joci? Prietenii te-au abandonat?
-                    </p>
-                    <p>
-                        Aici si acum poti sa te alaturi unor oameni alaturi de
-                        care sa iti dai in cap.
-                    </p>
-                </div>
-
-                {#if !user}
-                    <div>
-                        <Btn on:click={() => navigate('login')}>
-                            Intra in cont
-                        </Btn>
-                        <Btn on:click={() => navigate('register')}>
-                            Creeaza cont
-                        </Btn>
-                    </div>
-                {/if}
-            </div>
+        <div class="Home__content-wrapper__text-wrapper">
+            <p>
+                Esti intr-o cautare disperata de oameni? Nu ai cu cine sa te
+                joci? Prietenii te-au abandonat?
+            </p>
+            <p>
+                Aici si acum poti sa te alaturi unor oameni alaturi de care sa
+                iti dai in cap.
+            </p>
         </div>
-    </FullpageSection>
 
-    <FullpageSection>
-        <PageWrapper>
-            <h1>Echipe in cautare de playeri</h1>
-            <TeamsList />
-        </PageWrapper>
-    </FullpageSection>
-</Fullpage>
+        {#if !user}
+            <div>
+                <Btn on:click={() => navigate('login')}>Intra in cont</Btn>
+                <Btn on:click={() => navigate('register')}>Creeaza cont</Btn>
+            </div>
+        {/if}
+    </div>
+</div>
 
 <style>
     .Home {
+        margin-top: calc(var(--size-s) * -1);
+        margin-inline: calc(var(--spacing-m) * -1);
         background: url('../assets/imgs/gaming_bg_3.jpg') no-repeat center/cover;
     }
 

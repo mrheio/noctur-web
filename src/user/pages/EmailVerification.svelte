cd857b2b860666c69048a3ba4b693858f0cd0f1a
DrgOv <62381817+DrgOv@users.noreply.github.com>
2022-03-29 14:23:12 +0300

    fixed token refresh

--- src/user/pages/EmailVerification.svelte
+++ src/user/pages/EmailVerification.svelte
@@ -1,34 +1,49 @@
 <script>
-    import { sendEmailVerification } from 'firebase/auth';
+    import { getIdToken, sendEmailVerification } from 'firebase/auth';
     import { onDestroy, onMount } from 'svelte';
     import { navigate } from 'svelte-routing';
     import { authService } from '../../auth/data/authService';
+    import { Btn, FlexColumn } from '../../common/components';
     import { firebaseAuth } from '../../common/firebase';
 
-    let verified = false;
+    let verified = firebaseAuth.currentUser.emailVerified;
 
     const intervalId = setInterval(async () => {
         await authService.refreshUser();
         verified = authService.isEmailVerified();
-    }, 1500);
+    }, 5000);
 
-    $: {
-        if (verified) {
-            navigate('/', { replace: true });
-        }
-    }
-
-    onMount(async () => {
+    const sendEmail = async () => {
         await sendEmailVerification(firebaseAuth.currentUser, {
             url: 'https://iechipele.web.app',
         });
+    };
+
+    onMount(async () => {
+        await sendEmail();
     });
 
     onDestroy(() => {
         clearInterval(intervalId);
     });
+
+    $: {
+        if (verified) {
+            getIdToken(firebaseAuth.currentUser, true);
+            navigate('/', { replace: true });
+        }
+    }
 </script>
 
-<div class="fullheight flex-centered">
-    <h1>Trebuie sa iti verifici emailul</h1>
-</div>
+{#if !verified}
+    <div class="fullheight flex-centered">
+        <FlexColumn centered>
+            <h1>Trebuie sa iti verifici emailul</h1>
+            <Btn type="button" on:click={sendEmail}>Retrimite verificare</Btn>
+        </FlexColumn>
+    </div>
+{:else}
+    <div class="fullheight flex-centered">
+        <h1>Email verificat. Da refresh la pagina.</h1>
+    </div>
+{/if}
